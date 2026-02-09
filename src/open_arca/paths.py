c63d7513e1c416ad02688fb21a8f7dff323748fa
agustin herrera <agustinherrera.dev@gmail.com>
2026-02-09 18:20:25 -0300

    CREATE paths module for project dir and files management; CREATE logs, credentials and credentials/testing dirs with .keep files

---
+++ src/open_arca/paths.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+
+class Paths:
+    """
+    Administra la creación y obtención de las rutas disponibles en el sistema
+    """
+
+    def __init__(self):
+        self._root = Path(__file__).resolve().parent
+        self._logs = self._root / "logs"
+        self._credentials = self._root / "credentials"
+        self._credentials_testing = self._credentials / "testing"
+
+        self.dirs = [self._logs, self._credentials, self._credentials_testing]
+
+        self.create_dirs()
+        self.create_keeps()
+
+    @property
+    def root(self):
+        return self._root
+
+    @property
+    def logs(self):
+        return self._logs
+
+    @property
+    def credentials(self):
+        return self._credentials
+
+    @property
+    def credentials_testing(self):
+        return self._credentials_testing
+
+    def create_dirs(self):
+        for dir in self.dirs:
+            dir.mkdir(exist_ok=True)
+
+    def create_keeps(self):
+        for dir in self.dirs:
+            Path(dir / ".keep").touch(exist_ok=True)
+
+paths = Paths()
