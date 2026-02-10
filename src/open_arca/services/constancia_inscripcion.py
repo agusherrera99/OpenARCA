f40eb8bd8e9cc1b997e3f47f330ddee6d6129bfa
agustin herrera <agustinherrera.dev@gmail.com>
2026-02-10 18:06:36 -0300

    ADD obtener_informacion public method to ConstanciaIncripcion class; SET homologación wsdl and service_name variables

--- src/open_arca/services/constancia_inscripcion.py
+++ src/open_arca/services/constancia_inscripcion.py
@@ -3,14 +3,30 @@ logger = logging.getLogger(__name__)
 
 from zeep import Client
 
+from ..wsaa import Homologacion
+
+
+# NOTE: Por ahora solo trabajamos con homologación, más adelante agrego producción
 class ConstanciaInscripcion:
-    def __init__(self, testing: bool = True):
-        self.testing = testing
-        self.wsdl = "https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA5?WSDL" if self.testing else "https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA5?WSDL"
+    def __init__(self):
+        self.wsdl = "https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA5?WSDL"
         self.client = Client(self.wsdl)
+        self.service_name: str = "ws_sr_constancia_inscripcion"
 
     def dummy(self):
         logger.info("Chequeando dummy...")
         response = self.client.service.dummy()
         logger.info(response)
 
+        return response
+
+    def obtener_información(self, cuit: str):
+        homologacion = Homologacion("Agustín Herrera", "OpenARCAtest", "20419264300")
+        token, sign = homologacion.get_ticket_access_authentications(self.service_name)
+        result = self.client.service.getPersona_V2(
+            token=token,
+            sign=sign,
+            cuitRepresentada=homologacion.serial_number,
+            idPersona=cuit
+        )
+        return result
