f84f75fcc300353fdfd01e1c6d7b4f88e0690619
agusherrera99 <agustinherrera.dev@gmail.com>
2026-02-13 13:56:36 -0300

    CREATE OpenARCA main class with first available service (constancia_inscripcion); UPDATE project version to 0.1.5

--- src/open_arca/__init__.py
+++ src/open_arca/__init__.py
@@ -1,7 +1,26 @@
 import logging
 logger = logging.getLogger(__name__)
 
-from .routes import LogPath
+from .routes import LogPath, CredentialPath
+from .services import ConstanciaInscripcion
+from .wsaa import get_wsaa_client
+
+
+class OpenARCA:
+    def __init__(
+        self,
+        organization_name: str,
+        common_name: str,
+        serial_number: int,
+        testing: bool = False
+    ):
+        self.wsaa_instance = get_wsaa_client(
+            testing,
+            organization_name=organization_name,
+            common_name=common_name,
+            serial_number=serial_number
+        )
+        self.constancia_inscripcion = ConstanciaInscripcion(self.wsaa_instance, testing=testing)
 
 
 def main() -> None:
